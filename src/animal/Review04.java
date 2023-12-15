6f775101a0b7c7b1a149c12d8884bb8a0fd99e79
kimkimcamera <kimkimcamera@gmail.com>
2023-12-15 20:05:46 +0900

    レビュー基準を満たしていない所を 私なりに修正してみました。

--- src/animal/Review04.java
+++ src/animal/Review04.java
@@ -4,27 +4,18 @@ public class Review04 {
 
     public static void main(String[] args) {
         
-        Animal animal01 = new Animal("田中 太郎", 25);
-        animal01.say();
         
-        Human hobby01 = new Human();
-        hobby01.setHobby("電車");
-        hobby01.think ();
-
+        Human tanaka = new Human("田中 太郎", 25, "電車");
+        tanaka.say();
+        tanaka.think ();
         
-        Animal animal02 = new Animal("鈴木 次郎", 30);
-        animal02.say();
+        Human suzuki = new Human("鈴木 次郎", 30, "野球");
+        suzuki.say();
+        suzuki.think ();
         
-        Human hobby02 = new Human();
-        hobby02.setHobby("野球");
-        hobby02.think ();
-        
-        Animal animal03 = new Animal("佐藤 花子", 20);
-        animal03.say();
-        
-        Human hobby03 = new Human();
-        hobby03.setHobby ("映画");
-        hobby03.think ();
+        Human sato = new Human("佐藤 花子", 20, "映画");
+        sato.say();
+        sato.think ();
 
     }
 
